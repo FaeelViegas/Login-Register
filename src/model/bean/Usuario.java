4e8e86dbc6432b5e5eccb92018b8843709b807c3
Victor Rafaeel <rafaeel410@gmail.com>
2023-11-15 16:49:13 -0300

    Cria metodos getter e setter da classe usuario

--- src/model/bean/Usuario.java
+++ src/model/bean/Usuario.java
@@ -10,5 +10,51 @@ package model.bean;
  * @author Usuario
  */
 public class Usuario {
+    private int idUsuario;
+    private String userName;
+    private String email;
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String userName, String email, String senha) {
+        this.idUsuario = idUsuario;
+        this.userName = userName;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
     
 }
